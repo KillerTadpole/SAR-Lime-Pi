b7abce07c119bb26efe5e1952ea4f2cac10859a3
Charles Merritt <chazzmerritt2@gmail.com>
2022-02-02 17:08:26 -0700

    Working on getting gr-lime block for gnuradio built. Still in the middle of debugging the make command.

--- Docker_stuff/podman-gui-enabled.sh
+++ Docker_stuff/podman-gui-enabled.sh
@@ -1,10 +1,19 @@
 
 # run interactive podman with gui enabled
+SAR_LIME_PI_DIR=~/Documents/sar_project/SAR-Lime-Pi
 
-podman run -it --rm -e DISPLAY \
-	   -v /tmp/.X11-unix:/tmp/.X11-unix \
-	   --security-opt label=type:container_runtime_t $1
-
+podman run -it \
+	--rm \
+	-e DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	--network=host \
+	--privileged \
+	--device=/dev/steam-2-3 \
+	-v /dev:/dev \
+	-v $SAR_LIME_PI_DIR:/home/SAR-Lime-Pi \
+	--group-add keep-groups\
+	--security-opt label=type:container_runtime_t \
+	$1
 
 
 
